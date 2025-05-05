2e25d1a2d10c68063e4657a625c3e08c59ed535f
KaruroChori <KaruroChori@users.noreply.github.com>
2025-05-05 18:06:52 +0000

    deploy: 7fc6d7dab070c861d8683c9fca08e80eb3e812de

--- next/doxygen/search/all_9.js
+++ next/doxygen/search/all_9.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['text_5ft_0',['text_t',['../structxml_1_1text__t.html',1,'xml']]],
   ['tree_1',['Tree',['../structxml_1_1Tree.html',1,'xml']]],
-  ['tree_2ehpp_2',['tree.hpp',['../tree_8hpp.html',1,'']]]
+  ['tree_2ehpp_2',['tree.hpp',['../tree_8hpp.html',1,'']]],
+  ['tree_5fptr_5ft_3',['tree_ptr_t',['../structxml_1_1tree__ptr__t.html',1,'xml']]]
 ];
--- next/doxygen/search/classes_8.js
+++ next/doxygen/search/classes_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['text_5ft_0',['text_t',['../structxml_1_1text__t.html',1,'xml']]],
-  ['tree_1',['Tree',['../structxml_1_1Tree.html',1,'xml']]]
+  ['tree_1',['Tree',['../structxml_1_1Tree.html',1,'xml']]],
+  ['tree_5fptr_5ft_2',['tree_ptr_t',['../structxml_1_1tree__ptr__t.html',1,'xml']]]
 ];
