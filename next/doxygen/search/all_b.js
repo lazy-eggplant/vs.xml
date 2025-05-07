a1101be3efd849e5be6c60f1a732865bc72e5437
KaruroChori <KaruroChori@users.noreply.github.com>
2025-05-07 11:49:49 +0000

    deploy: 2ff3430c59b9d4867786ac309c1073e255652e89

--- next/doxygen/search/all_b.js
+++ next/doxygen/search/all_b.js
@@ -2,6 +2,9 @@ var searchData=
 [
   ['wrp_5fattr_5fiterator_0',['wrp_attr_iterator',['../structxml_1_1wrp__attr__iterator.html',1,'xml']]],
   ['wrp_5fbase_5ft_1',['wrp_base_t',['../structxml_1_1wrp__base__t.html',1,'xml']]],
-  ['wrp_5fnode_5fiterator_2',['wrp_node_iterator',['../structxml_1_1wrp__node__iterator.html',1,'xml']]],
-  ['wrptree_3',['WrpTree',['../structxml_1_1WrpTree.html',1,'xml']]]
+  ['wrp_5fbase_5ft_3c_20attr_5ft_20_3e_2',['wrp_base_t&lt; attr_t &gt;',['../structxml_1_1wrp__base__t_3_01attr__t_01_4.html',1,'xml']]],
+  ['wrp_5fbase_5ft_3c_20xml_3a_3aattr_5ft_20_3e_3',['wrp_base_t&lt; xml::attr_t &gt;',['../structxml_1_1wrp__base__t.html',1,'xml']]],
+  ['wrp_5fbase_5ft_3c_20xml_3a_3aunknown_5ft_20_3e_4',['wrp_base_t&lt; xml::unknown_t &gt;',['../structxml_1_1wrp__base__t.html',1,'xml']]],
+  ['wrp_5fnode_5fiterator_5',['wrp_node_iterator',['../structxml_1_1wrp__node__iterator.html',1,'xml']]],
+  ['wrptree_6',['WrpTree',['../structxml_1_1WrpTree.html',1,'xml']]]
 ];
