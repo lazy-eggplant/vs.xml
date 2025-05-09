af63f5f01883b519fbb0d1bd1b98d9b916b7d6b5
KaruroChori <KaruroChori@users.noreply.github.com>
2025-05-09 04:28:14 +0000

    deploy: 0f579989293f650de68a3061f2f3d4b5f046fe47

--- next/doxygen/search/all_6.js
+++ next/doxygen/search/all_6.js
@@ -1,8 +1,7 @@
 var searchData=
 [
-  ['parser_0',['Parser',['../structVS__XML__NS_1_1Parser.html',1,'VS_XML_NS']]],
+  ['parser_0',['Parser',['../classVS__XML__NS_1_1Parser.html',1,'VS_XML_NS']]],
   ['parser_2ehpp_1',['parser.hpp',['../parser_8hpp.html',1,'']]],
-  ['parsercompressed_2',['ParserCompressed',['../structVS__XML__NS_1_1ParserCompressed.html',1,'VS_XML_NS']]],
-  ['print_5fcfg_5ft_3',['print_cfg_t',['../structVS__XML__NS_1_1Tree_1_1print__cfg__t.html',1,'VS_XML_NS::Tree']]],
-  ['proc_5ft_4',['proc_t',['../structVS__XML__NS_1_1proc__t.html',1,'VS_XML_NS']]]
+  ['print_5fcfg_5ft_2',['print_cfg_t',['../structVS__XML__NS_1_1Tree_1_1print__cfg__t.html',1,'VS_XML_NS::Tree']]],
+  ['proc_5ft_3',['proc_t',['../structVS__XML__NS_1_1proc__t.html',1,'VS_XML_NS']]]
 ];
--- next/doxygen/search/classes_6.js
+++ next/doxygen/search/classes_6.js
@@ -1,7 +1,6 @@
 var searchData=
 [
-  ['parser_0',['Parser',['../structVS__XML__NS_1_1Parser.html',1,'VS_XML_NS']]],
-  ['parsercompressed_1',['ParserCompressed',['../structVS__XML__NS_1_1ParserCompressed.html',1,'VS_XML_NS']]],
-  ['print_5fcfg_5ft_2',['print_cfg_t',['../structVS__XML__NS_1_1Tree_1_1print__cfg__t.html',1,'VS_XML_NS::Tree']]],
-  ['proc_5ft_3',['proc_t',['../structVS__XML__NS_1_1proc__t.html',1,'VS_XML_NS']]]
+  ['parser_0',['Parser',['../classVS__XML__NS_1_1Parser.html',1,'VS_XML_NS']]],
+  ['print_5fcfg_5ft_1',['print_cfg_t',['../structVS__XML__NS_1_1Tree_1_1print__cfg__t.html',1,'VS_XML_NS::Tree']]],
+  ['proc_5ft_2',['proc_t',['../structVS__XML__NS_1_1proc__t.html',1,'VS_XML_NS']]]
 ];
