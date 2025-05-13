e6f23740b8434b6f1aff6f87215a00eb66b21fba
KaruroChori <KaruroChori@users.noreply.github.com>
2025-05-13 18:41:24 +0000

    deploy: a63693d30dbafc714cdf5890595f2c0621294c3b

--- next/doxygen/search/all_2.js
+++ next/doxygen/search/all_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['cdata_5ft_0',['cdata_t',['../structVS__XML__NS_1_1cdata__t.html',1,'VS_XML_NS']]],
-  ['clone_1',['clone',['../structVS__XML__NS_1_1Tree.html#ad284ee5077be5fb1003a94c2b6aed70a',1,'VS_XML_NS::Tree']]],
+  ['clone_1',['clone',['../structVS__XML__NS_1_1Tree.html#a3dc4d71468f7a3c1b561ae941fe0bc25',1,'VS_XML_NS::Tree']]],
   ['comment_5ft_2',['comment_t',['../structVS__XML__NS_1_1comment__t.html',1,'VS_XML_NS']]]
 ];
--- next/doxygen/search/functions_0.js
+++ next/doxygen/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['clone_0',['clone',['../structVS__XML__NS_1_1Tree.html#ad284ee5077be5fb1003a94c2b6aed70a',1,'VS_XML_NS::Tree']]]
+  ['clone_0',['clone',['../structVS__XML__NS_1_1Tree.html#a3dc4d71468f7a3c1b561ae941fe0bc25',1,'VS_XML_NS::Tree']]]
 ];
