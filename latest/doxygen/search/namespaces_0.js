c617c74170d1a7e203ff7bb83ce3e9f719c1e680
KaruroChori <KaruroChori@users.noreply.github.com>
2025-05-16 09:40:37 +0000

    deploy: 5f030f0c569baa0dd85fcc7996105ad64ace6ad2

---
+++ latest/doxygen/search/namespaces_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['vs_5fxml_5fns_3a_3afilters_0',['filters',['../namespaceVS__XML__NS_1_1filters.html',1,'VS_XML_NS']]]
+];
--- latest/doxygen/search/searchdata.js
+++ latest/doxygen/search/searchdata.js
@@ -2,26 +2,29 @@ var indexSectionsWithContent =
 {
   0: "abcdeilmnprstuvw",
   1: "abcdelmnprstu",
-  2: "bdipstw",
-  3: "cirst",
-  4: "v"
+  2: "v",
+  3: "bdipstw",
+  4: "cirst",
+  5: "v"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "classes",
-  2: "files",
-  3: "functions",
-  4: "concepts"
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "concepts"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Classes",
-  2: "Files",
-  3: "Functions",
-  4: "Concepts"
+  2: "Namespaces",
+  3: "Files",
+  4: "Functions",
+  5: "Concepts"
 };
 
