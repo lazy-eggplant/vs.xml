f0f6c95a1965b26a35f9d66c9b2919245c8c052b
KaruroChori <KaruroChori@users.noreply.github.com>
2025-05-17 09:31:10 +0000

    deploy: 2d9ac3e78d42821d88bffb21e37731e4a7d4e333

--- next/doxygen/search/classes_5.js
+++ next/doxygen/search/classes_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['formatter_3c_20xml_3a_3awrp_3a_3asv_2c_20char_20_3e_0',['formatter&lt; xml::wrp::sv, char &gt;',['../structstd_1_1formatter_3_01xml_1_1wrp_1_1sv_00_01char_01_4.html',1,'std']]]
+  ['formatter_3c_20vs_5fxml_5fns_3a_3awrp_3a_3asv_2c_20char_20_3e_0',['formatter&lt; VS_XML_NS::wrp::sv, char &gt;',['../structstd_1_1formatter_3_01VS__XML__NS_1_1wrp_1_1sv_00_01char_01_4.html',1,'std']]]
 ];
