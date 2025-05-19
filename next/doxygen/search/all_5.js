0f795de2a49eebe8ab27e409188a1d252467d778
KaruroChori <KaruroChori@users.noreply.github.com>
2025-05-19 01:23:56 +0000

    deploy: 169187761d34b2fe0e6bedc80d208aeab925610d

--- next/doxygen/search/all_5.js
+++ next/doxygen/search/all_5.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['element_5ft_0',['element_t',['../structVS__XML__NS_1_1element__t.html',1,'VS_XML_NS']]],
-  ['escaped_5fview_1',['escaped_view',['../classVS__XML__NS_1_1serialize_1_1escaped__view.html',1,'VS_XML_NS::serialize']]],
-  ['exp_2',['exp',['../structVS__XML__NS_1_1query_1_1exp.html',1,'VS_XML_NS::query']]]
+  ['error_5ft_1',['error_t',['../structVS__XML__NS_1_1Parser_1_1error__t.html',1,'VS_XML_NS::Parser']]],
+  ['escaped_5fview_2',['escaped_view',['../classVS__XML__NS_1_1serialize_1_1escaped__view.html',1,'VS_XML_NS::serialize']]],
+  ['exp_3',['exp',['../structVS__XML__NS_1_1query_1_1exp.html',1,'VS_XML_NS::query']]]
 ];
