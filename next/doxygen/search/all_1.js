8c1edfbeed2794eeeae96338294b4471eb922f40
KaruroChori <KaruroChori@users.noreply.github.com>
2025-05-19 12:15:55 +0000

    deploy: ae2e8299851fd5c3bb38ed93f6817a4bd483c42c

--- next/doxygen/search/all_1.js
+++ next/doxygen/search/all_1.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['assert_2ehpp_0',['assert.hpp',['../assert_8hpp.html',1,'']]],
   ['attr_5fiterator_1',['attr_iterator',['../structVS__XML__NS_1_1attr__iterator.html',1,'VS_XML_NS::attr_iterator'],['../structVS__XML__NS_1_1wrp_1_1attr__iterator.html',1,'VS_XML_NS::wrp::attr_iterator']]],
-  ['attr_5ft_2',['attr_t',['../structVS__XML__NS_1_1attr__t.html',1,'VS_XML_NS']]]
+  ['attr_5ft_2',['attr_t',['../structVS__XML__NS_1_1attr__t.html',1,'VS_XML_NS::attr_t'],['../structVS__XML__NS_1_1TreeBuilder_1_1attr__t.html',1,'VS_XML_NS::TreeBuilder&lt; cfg &gt;::attr_t']]]
 ];
