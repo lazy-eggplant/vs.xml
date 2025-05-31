651f67258ae744d042949a5a32bec3f2ec463585
KaruroChori <KaruroChori@users.noreply.github.com>
2025-05-31 09:03:49 +0000

    deploy: 0c391ec94cd4092036adcd402932e3fe9f3f79d8

--- next/doxygen/search/all_3.js
+++ next/doxygen/search/all_3.js
@@ -3,6 +3,6 @@ var searchData=
   ['cdata_5ft_0',['cdata_t',['../structVS__XML__NS_1_1cdata__t.html',1,'VS_XML_NS']]],
   ['clone_1',['clone',['../structVS__XML__NS_1_1TreeRaw.html#a2c362df4058a9d0fd145605dc88aa347',1,'VS_XML_NS::TreeRaw']]],
   ['close_2',['close',['../structVS__XML__NS_1_1TreeBuilder.html#ad2fd742da45362ef4c32e3d59d5ccaab',1,'VS_XML_NS::TreeBuilder']]],
-  ['close_5fframe_3',['close_frame',['../structVS__XML__NS_1_1TreeBuilder.html#af143ebd95656a501d1c8a015b1047eef',1,'VS_XML_NS::TreeBuilder']]],
+  ['close_5fframe_3',['close_frame',['../structVS__XML__NS_1_1TreeBuilder.html#af8475fd92faffc7ac1e50d87e89710a2',1,'VS_XML_NS::TreeBuilder']]],
   ['comment_5ft_4',['comment_t',['../structVS__XML__NS_1_1comment__t.html',1,'VS_XML_NS']]]
 ];
--- next/doxygen/search/classes_f.js
+++ next/doxygen/search/classes_f.js
@@ -6,8 +6,7 @@ var searchData=
   ['token_5ft_3',['token_t',['../structVS__XML__NS_1_1query_1_1token__t.html',1,'VS_XML_NS::query']]],
   ['tree_4',['Tree',['../structVS__XML__NS_1_1Tree.html',1,'VS_XML_NS']]],
   ['treebuilder_5',['TreeBuilder',['../structVS__XML__NS_1_1TreeBuilder.html',1,'VS_XML_NS']]],
-  ['treebuilder_3c_20cfg_20_3e_6',['TreeBuilder&lt; cfg &gt;',['../structVS__XML__NS_1_1TreeBuilder.html',1,'VS_XML_NS']]],
-  ['treebuilder_3c_20_7b_7d_20_3e_7',['TreeBuilder&lt; {} &gt;',['../structVS__XML__NS_1_1TreeBuilder.html',1,'VS_XML_NS']]],
-  ['treeraw_8',['TreeRaw',['../structVS__XML__NS_1_1TreeRaw.html',1,'VS_XML_NS']]],
-  ['type_5ffilter_5ft_9',['type_filter_t',['../structVS__XML__NS_1_1query_1_1Token_1_1type__filter__t.html',1,'VS_XML_NS::query::Token::type_filter_t&lt; T &gt;'],['../structVS__XML__NS_1_1query_1_1token__t_1_1type__filter__t.html',1,'VS_XML_NS::query::token_t::type_filter_t&lt; T &gt;']]]
+  ['treebuilder_3c_20_7b_7d_20_3e_6',['TreeBuilder&lt; {} &gt;',['../structVS__XML__NS_1_1TreeBuilder.html',1,'VS_XML_NS']]],
+  ['treeraw_7',['TreeRaw',['../structVS__XML__NS_1_1TreeRaw.html',1,'VS_XML_NS']]],
+  ['type_5ffilter_5ft_8',['type_filter_t',['../structVS__XML__NS_1_1query_1_1Token_1_1type__filter__t.html',1,'VS_XML_NS::query::Token::type_filter_t&lt; T &gt;'],['../structVS__XML__NS_1_1query_1_1token__t_1_1type__filter__t.html',1,'VS_XML_NS::query::token_t::type_filter_t&lt; T &gt;']]]
 ];
