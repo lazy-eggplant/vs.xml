66bee30e8ae73d77fe70a266f5551a89f7d744c8
KaruroChori <KaruroChori@users.noreply.github.com>
2025-05-31 17:58:47 +0000

    deploy: 77a2e90a9b06b0b76e6a485415b1a4eda687585d

--- next/doxygen/search/all_6.js
+++ next/doxygen/search/all_6.js
@@ -3,5 +3,5 @@ var searchData=
   ['fork_0',['fork',['../structVS__XML__NS_1_1TreeBuilder.html#a5bfb45cd25b753c47fce4dfc971bf29e',1,'VS_XML_NS::TreeBuilder']]],
   ['format_2ehpp_1',['format.hpp',['../format_8hpp.html',1,'']]],
   ['formatter_3c_20vs_5fxml_5fns_3a_3awrp_3a_3asv_2c_20char_20_3e_2',['formatter&lt; VS_XML_NS::wrp::sv, char &gt;',['../structstd_1_1formatter_3_01VS__XML__NS_1_1wrp_1_1sv_00_01char_01_4.html',1,'std']]],
-  ['from_5fbinary_5ferror_5ft_3',['from_binary_error_t',['../structVS__XML__NS_1_1ArchiveRaw_1_1from__binary__error__t.html',1,'VS_XML_NS::ArchiveRaw::from_binary_error_t'],['../structVS__XML__NS_1_1TreeRaw_1_1from__binary__error__t.html',1,'VS_XML_NS::TreeRaw::from_binary_error_t']]]
+  ['from_5fbinary_5ferror_5ft_3',['from_binary_error_t',['../structVS__XML__NS_1_1TreeRaw_1_1from__binary__error__t.html',1,'VS_XML_NS::TreeRaw']]]
 ];
--- next/doxygen/search/functions_1.js
+++ next/doxygen/search/functions_1.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['clone_0',['clone',['../structVS__XML__NS_1_1TreeRaw.html#a2c362df4058a9d0fd145605dc88aa347',1,'VS_XML_NS::TreeRaw']]],
   ['close_1',['close',['../structVS__XML__NS_1_1TreeBuilder.html#ad2fd742da45362ef4c32e3d59d5ccaab',1,'VS_XML_NS::TreeBuilder']]],
-  ['close_5fframe_2',['close_frame',['../structVS__XML__NS_1_1TreeBuilder.html#a2eec3578baf35f8c2618bf48d57f6f55',1,'VS_XML_NS::TreeBuilder']]]
+  ['close_5fframe_2',['close_frame',['../structVS__XML__NS_1_1TreeBuilder.html#ac500291140f5825094e423a4c06f50a8',1,'VS_XML_NS::TreeBuilder']]]
 ];
