20566525fb7c383e7c4555b47eafdf8576a6babf
KaruroChori <KaruroChori@users.noreply.github.com>
2025-05-31 15:35:47 +0000

    deploy: 0e4e210dec521dbe50e458aadbc5fc816718eef1

--- next/doxygen/search/functions_1.js
+++ next/doxygen/search/functions_1.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['clone_0',['clone',['../structVS__XML__NS_1_1TreeRaw.html#a2c362df4058a9d0fd145605dc88aa347',1,'VS_XML_NS::TreeRaw']]],
   ['close_1',['close',['../structVS__XML__NS_1_1TreeBuilder.html#ad2fd742da45362ef4c32e3d59d5ccaab',1,'VS_XML_NS::TreeBuilder']]],
-  ['close_5fframe_2',['close_frame',['../structVS__XML__NS_1_1TreeBuilder.html#af8475fd92faffc7ac1e50d87e89710a2',1,'VS_XML_NS::TreeBuilder']]]
+  ['close_5fframe_2',['close_frame',['../structVS__XML__NS_1_1TreeBuilder.html#a2eec3578baf35f8c2618bf48d57f6f55',1,'VS_XML_NS::TreeBuilder']]]
 ];
