b369f96c5ba3f1fa5a1ebde7f429cf9cbcb5da3a
KaruroChori <KaruroChori@users.noreply.github.com>
2025-06-21 17:33:30 +0100

    CLI utils boilerplate and experiments (#24)
    * Minor fixes
    
    * Added some tests and boilerplate code for the future archive builder to use via CLI..

---
+++ include/vs-xml/tree-iterator.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+/**
+ * @file tree-iterator.hpp
+ * @author karurochari
+ * @brief Stackless iterator able to navigate a tree structure.
+ * @date 2025-06-21
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
