208b5731bdf5d5e0f7eb23a92efcb3178e68e680
Karuro Chari <karurochori@users.noreply.github.com>
2025-06-26 12:23:00 +0100

    More boilerplate for the QueryBuilder

--- lib/query-builder.cpp
+++ lib/query-builder.cpp
@@ -3,6 +3,20 @@
 namespace VS_XML_NS{
 namespace query{
 
+QueryBuilder::error_t QueryBuilder::begin_frame(std::string_view name, Type type){return error_t::NOT_IMPLEMENTED;}
+QueryBuilder::error_t QueryBuilder::end_frame(){return error_t::NOT_IMPLEMENTED;}
+
+QueryBuilder::error_t QueryBuilder::any(std::string_view capture){return error_t::NOT_IMPLEMENTED;}
+
+QueryBuilder::error_t QueryBuilder::begin(std::string_view capture){return error_t::NOT_IMPLEMENTED;}
+QueryBuilder::error_t QueryBuilder::end(){return error_t::NOT_IMPLEMENTED;}
+
+QueryBuilder::error_t QueryBuilder::match_type(Token::type_filter_t<Token::type_t::MATCH_TYPE> expr){return error_t::NOT_IMPLEMENTED;}
+QueryBuilder::error_t QueryBuilder::match_ns(Token::single_t<Token::type_t::MATCH_NS> expr){return error_t::NOT_IMPLEMENTED;}
+QueryBuilder::error_t QueryBuilder::match_name(Token::single_t<Token::type_t::MATCH_NAME> expr){return error_t::NOT_IMPLEMENTED;}
+QueryBuilder::error_t QueryBuilder::match_value(Token::single_t<Token::type_t::MATCH_VALUE> expr){return error_t::NOT_IMPLEMENTED;}
+QueryBuilder::error_t QueryBuilder::match_all_text(Token::single_t<Token::type_t::MATCH_ALL_TEXT> expr){return error_t::NOT_IMPLEMENTED;}
+QueryBuilder::error_t QueryBuilder::match_attr(Token::attr_t<Token::type_t::MATCH_ATTR> expr, std::string_view capture){return error_t::NOT_IMPLEMENTED;}
 
 }
 }
---
+++ test/src/query-builder.cpp
@@ -0,0 +1,9 @@
+#include <vs-xml/query-builder.hpp>
+
+int main(){
+    using namespace xml::query;
+
+    QueryBuilder bld;
+
+    return 0;
+}
